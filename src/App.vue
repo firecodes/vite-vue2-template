42313c1b9c9b3a4f7cda3592cbd7833f0692a035
firecodes <384502958@qq.com>
2023-11-06 19:34:51 +0800

    修改链接

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <template v-for="(route, index) in routersList">
-      <el-link :key="index" class="link" :href="`/#${route.path}`">{{ route.name }}</el-link>
+      <el-link :key="index" class="link" :href="`./#${route.path}`">{{ route.name }}</el-link>
     </template>
     <router-view />
   </div>
