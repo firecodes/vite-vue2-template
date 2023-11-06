50b59748f6eb6af532d9c4887714d4c9d0c54bf7
firecodes <384502958@qq.com>
2023-11-06 19:34:21 +0800

    发包

---
+++ assets/DemoMarkdown-7b36ab47.js
@@ -0,0 +1,1 @@
+import{n}from"./index-d8435b8a.js";const r={data(){return{article:{}}},computed:{defaultContent(){return""}},created(){},methods:{}};var a=function(){var t=this,e=t._self._c;return e("div",{staticClass:"about"},[e("markdown",{attrs:{html:t.defaultContent}})],1)},o=[],_=n(r,a,o,!1,null,null,null,null);const c=_.exports;export{c as default};
